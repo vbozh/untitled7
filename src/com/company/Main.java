0902ec0e6c4c59710c19f722893cb75f0b415664
Viktoria Bozhko <viktoria.bozhko@jetbrains.com>
2020-01-24 13:35:25 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //IntelliJ IDEA 2019.3.1 (Community Edition)
+        //Build #IC-193.5662.53, built on December 18, 2019
+        //Runtime version: 11.0.5+10-b520.17 amd64
+        //VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o
+        //Windows 10 10.0
+        //GC: ParNew, ConcurrentMarkSweep
+        //Memory: 2000M
+        //Cores: 4
+        //Registry:
+        //Non-Bundled Plugins: PythonCore, org.intellij.scala
+    }
+}
